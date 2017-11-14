835c8e7aa6473b89d553871e5db308f31effe3ed
Your Name <you@example.com>
2017-11-14 11:39:14 +0000

    add two demos together

--- main.py
+++ main.py
@@ -2,16 +2,18 @@
 from importlib import import_module
 import os,time
 from flask import Flask, render_template, Response
-from websocket import startWebSocketServer 
+from websocket import startWebSocketServer
 
-from camera_opencv import *
+
+#from camera_opencv import *
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     """Video streaming home page."""
-    return render_template('index.html')
+    #return render_template('index_opencv.html')
+    return render_template('index_web.html')
 
 
 def gen(camera):
@@ -19,7 +21,7 @@ def gen(camera):
     while True:
         frame = camera.get_frame()
         yield (b'--frame\r\n'
-               b'Content-Type: image/main\r\n\r\n' + frame + b'\r\n')
+               b'Content-Type: image/png\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/videoel')
 def video_feed():
@@ -30,4 +32,5 @@ def video_feed():
 if __name__ == '__main__':
     startWebSocketServer()
     app.run(host='0.0.0.0', threaded=True)
+    #app.run(host='0.0.0.0', threaded=True, ssl_context='adhoc')
 
