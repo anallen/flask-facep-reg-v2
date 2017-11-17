5075c89d00c6ba3388f13778776fa26b83dfd654
Guo Feng-B41466 <feng.guo@freescale.com>
2017-11-17 11:42:06 +0800

    training finish for server side

--- facerecogniton/facemodules_server.py
+++ facerecogniton/facemodules_server.py
@@ -14,61 +14,60 @@ import cv2
 from align_custom import AlignCustom
 from face_feature import FaceFeature
 from mtcnn_detect import MTCNNDetect
-from tf_graph import FaceRecGraph
 import argparse
 import sys, time
 import json
 import numpy as np
 from PIL import Image
 import StringIO
+import threading
 
-FRGraph = FaceRecGraph();
+
 aligner = AlignCustom();
-extract_feature = FaceFeature(FRGraph)
-face_detect = MTCNNDetect(FRGraph, scale_factor=2); #scale_factor, rescales image for faster detection
-feature_data_set = None
+extract_feature = FaceFeature()
+face_detect = MTCNNDetect(scale_factor=2); #scale_factor, rescales image for faster detection
+feature_data_set = {}
+person_images = {}
 
 
 
-class PersonModel:
-    def __init__(self, name):
-        self.name = name
-        self.person_imgs = {"Left" : [], "Right": [], "Center": []};
-        self.feature = None
-        self.images = []
+def training_start(name):
+    global person_images,feature_data_set
+    if (name in feature_data_set or name in person_images):
+        return False
 
-def training_start_local(name):
-    if(has_name(name)):
-        return None
-    return PersonModel(name)
+    person_images[name] = {"Left" : [], "Right": [], "Center": []}
+    return True
 
-def training_proframe_local(model, frame):
+def training_proframe(name, frame):
+    global person_images,feature_data_set
     rects, landmarks = face_detect.detect_face(frame, 80);  # min face size is set to 80x80
     for (i, rect) in enumerate(rects):
         aligned_frame, pos = aligner.align(160,frame,landmarks[i]);
-        model.person_imgs[pos].append(aligned_frame)
+        person_images[name][pos].append(aligned_frame)
     for (i,rect) in enumerate(rects):
         cv2.rectangle(frame,(rect[0],rect[1]),(rect[0] + rect[2],rect[1]+rect[3]),(255,0,0))
     return frame
 
-def __training_thread_local(model, callback):
+def __training_thread(name, callback):
+    global person_images,feature_data_set
     person_features = {"Left" : [], "Right": [], "Center": []};
-    for pos in model.person_imgs:
+    for pos in person_images[name]:
         person_features[pos] = [np.mean(extract_feature.get_features(
-                                         model.person_imgs[pos]),axis=0).tolist()]
+                                         person_images[name][pos]),axis=0).tolist()]
     if (feature_data_set is not None):
-        __save_person_features(model.name, person_features)
-    callback(model.name, person_features)
+        del person_images[name]
+        feature_data_set[name] = person_features;
+        f = open('./models/facerec_128D.txt', 'w');
+        f.write(json.dumps(feature_data_set))
+        f.close()
+    if callback:
+        callback()
 
-def training_finish_local(model, callback):
-    t = threading.Thread(target=__training_thread_local, args=(model, callback,))
+def training_finish(name, callback=None):
+    t = threading.Thread(target=__training_thread, args=(name, callback,))
     t.start()
-    return t
-
-def __save_person_features(name, features):
-    feature_data_set[name] = features;
-    f = open('./models/facerec_128D.txt', 'w');
-    f.write(json.dumps(feature_data_set))
+    return True
 
 def get_person_names():
     names = []
@@ -76,71 +75,21 @@ def get_person_names():
         names.append(name)
     return names
 
-def delete_person_name(name):
-    if (has_name(name)):
+def delete_module(name):
+    print feature_data_set
+    if (feature_data_set is not None and name in feature_data_set):
         del feature_data_set[name];
         f = open('./models/facerec_128D.txt', 'w');
         f.write(json.dumps(feature_data_set))
-
-import requests
-import threading
-
-url = 'http://10.193.20.74:8383/train'
-
-def __training_thread_remote(model, callback):
-    args = {'id': model.name, 'end':'true'}
-    headers = {"Content-type":"application/json","Accept": "application/json"}
-    files = {}
-    for i,f in enumerate(model.images):
-        files['file{}'.format(i)] = ('{}.png'.format(i), f, 'image/png')
-    r = requests.post(url, params=args, files=files)
-
-    args = {'id': model.name}
-    headers = {"Content-type":"application/json","Accept": "application/json"}
-    while (True):
-        time.sleep(1)
-        r = requests.get(url, params=args, headers=headers)
-        ret = json.loads(r.text)
-        if ('state'in ret and ret['state'] == 'FINISH'):
-            __save_person_features(model.name, ret['feature'])
-            callback(model.name, r.text)
-            headers = {"Content-type":"application/json","Accept": "application/json"}
-            r = requests.delete(url, params=args, headers=headers)
-            break
-
-def training_start_remote(name):
-    args = {'id': name}
-    headers = {"Content-type":"application/json","Accept": "application/json"}
-    r = requests.put(url, params=args, headers=headers)
-    return PersonModel(name)
-
-def training_proframe_remote(model, frame):
-    picf = StringIO.StringIO()
-    pi = Image.fromarray(frame)
-    pi.save(picf, format = "jpeg")
-    picf.seek(0)
-
-    model.images.append(picf)
-
-def training_finish_remote(model, callback):
-    t = threading.Thread(target=__training_thread_remote, args=(model, callback,))
-    t.start()
-    return t
-
-training_start = training_start_local
-training_proframe = training_proframe_local
-training_finish = training_finish_local
-
-def recog_engine_init(serverip=None):
-    global training_start, training_proframe, training_finish
-    if (serverip is not None):
-        global url
-        url = 'http://{}:8383/train'.format(serverip)
-        training_start = training_start_remote
-        training_proframe = training_proframe_remote
-        training_finish = training_finish_remote
-    global feature_data_set
+        f.close()
+        return True
+    else:
+        return False
 
 def load_modules():
+    global feature_data_set
     f = open('./models/facerec_128D.txt','r');
     feature_data_set = json.loads(f.read());
+    f.close()
+
+load_modules()
--- restapi_server.py
+++ restapi_server.py
@@ -1,95 +1,75 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # by vellhe 2017/7/9
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_restful import reqparse, abort, Api, Resource, request
 import werkzeug
 import os
 import StringIO
+import paho.mqtt.client as mqtt
+import numpy as np
+from PIL import Image
 
 
-import facerecogniton.face_reg as face_reg
+
+import facerecogniton.facemodules_server as facemodules
+parser = reqparse.RequestParser()
 
 app = Flask(__name__)
 api = Api(app)
-
-parser = reqparse.RequestParser()
 parser.add_argument('id', type=str, location='args')
 parser.add_argument('end', type=str, location='args')
 parser.add_argument('file', type=werkzeug.datastructures.FileStorage, location='files')
 
+mqttclient = mqtt.Client()
+mqttclient.connect('localhost', 1883, 60)
+mqttclient.loop_start()
 
-def remove_dir(dir):
-    if(os.path.isdir(dir)):
-        for p in os.listdir(dir):
-            remove_dir(os.path.join(dir,p))
-        if(os.path.exists(dir)):
-            os.rmdir(dir)
-    else:
-        if(os.path.exists(dir)):
-            os.remove(dir)
-
+modulespath = os.path.join(app.root_path,'models')
 class TrainModels(Resource):
-    modles = {}
-    def __add_modle(self, name):
-        if name not in self.modles:
-            self.modles[name] = {'state':'INIT', 'features':None, 'count':0}
-
-    def __del_modle(self, name):
-        if name in self.modles:
-            del self.modles[name]
-
-    def __get_modle_features(self, name):
-        if name not in self.modles:
-            ret = {'state':'NONAME'}
-        elif self.modles[name]['features'] is None:
-            ret = {'state':'TRAINING'}
-        else:
-            ret = {'state':'FINISH', 'feature':self.modles[name]['features']}
-        return ret
-
-    def __train_modle(self, name, images):
-        if name not in self.modles or len(images) == 0:
-            return
-        pmodel = face_reg.training_start(name)
-        for picf in images:
-            face_reg.training_proframe(pmodel, picf)
-        face_reg.training_finish(pmodel, self.__train_finish)
-
-    def __train_finish(self, name, features):
-        print '__train_finish'
-        if name in self.modles:
-            self.modles[name]['features'] = features
+    def __modules_updated(self):
+        mqttclient.publish("NXP_FACE_RECG_MODULES_UPDATED", "True")
 
     def get(self):
-        args = parser.parse_args()
-        ret = self.__get_modle_features(args['id'])
-        return ret, 200
+        return send_from_directory(modulespath, 'facerec_128D.txt', as_attachment=True)
 
     def delete(self):
         args = parser.parse_args()
-        self.__del_modle(args['id'])
-        return 'SUCCESS', 200
+        ret = facemodules.delete_module(args['id'])
+        if ret == True:
+            ret = {'state':'SUCCESS'}
+            self.__modules_updated()
+        elif ret == False:
+            ret = {'state':'FAILED'}
+        return ret, 200
 
     def put(self):
         args = parser.parse_args()
-        self.__add_modle(args['id'])
-        return 'SUCCESS', 201
+        ret = facemodules.training_start(args['id'])
+        if ret == True:
+            ret = {'state':'SUCCESS'}
+        elif ret == False:
+            ret = {'state':'FAILED'}
+        return ret, 201
 
     def post(self):
         args = parser.parse_args()
-        images = []
         for fname, f in request.files.items():
             pif=StringIO.StringIO()
             f.save(pif)
-            images.append(pif)
-        self.__train_modle(args['id'], images)
-        return 'SUCCESS', 201
+            pili = Image.open(pif)
+            frame = np.array(pili)
+            facemodules.training_proframe(args['id'], frame)
+        ret = facemodules.training_finish(args['id'], self.__modules_updated)
+        if ret == True:
+            ret = {'state':'SUCCESS'}
+        elif ret == False:
+            ret = {'state':'FAILED'}
+        return ret, 201
 
 ##
 ## Actually setup the Api resource routing here
 ##
 api.add_resource(TrainModels, '/train')
-
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8383, debug=True)
+    app.run(host="0.0.0.0", port=8383, debug=False)
